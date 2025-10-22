5cd5154ee79627c98c6381fcc641645d10aa38f2
imyj00 <108146596+imyj00@users.noreply.github.com>
2025-10-23 00:14:35 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 292 ms, Memory: 18408 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = N; i > 0; i --){
+            for(int k = 0; k < N-i; k++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
