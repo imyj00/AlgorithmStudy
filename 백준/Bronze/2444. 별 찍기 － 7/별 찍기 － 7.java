e17ae26b049e20b8dac31702d7ed68cb7c3e5ae9
imyj00 <108146596+imyj00@users.noreply.github.com>
2025-11-09 21:29:59 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 484 ms, Memory: 22040 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= num -1; i++){
+            for(int j = num - i; j >= 1; j--){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = num; i >= 1; i--){
+            for(int j = num-i; j > 0; j--){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+}
