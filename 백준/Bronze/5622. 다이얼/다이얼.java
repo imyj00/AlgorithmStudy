dec503781a3eae51de493c7ce245f0fbbd3cd938
imyj00 <108146596+imyj00@users.noreply.github.com>
2025-11-12 16:37:30 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int time = 0;
+
+        for(int i = 0; i < input.length(); i++){
+            if((input.charAt(i) == 'A') || (input.charAt(i) == 'B') || (input.charAt(i) =='C')){
+                time += 3;
+            }
+            else if((input.charAt(i) == 'D') || (input.charAt(i) == 'E') || (input.charAt(i) =='F')){
+                time += 4;
+            }
+            else if((input.charAt(i) == 'G') || (input.charAt(i) == 'H') || (input.charAt(i) =='I')){
+                time += 5;
+            }
+            else if((input.charAt(i) == 'J') || (input.charAt(i) == 'K') || (input.charAt(i) =='L')){
+                time += 6;
+            }
+            else if((input.charAt(i) == 'M') || (input.charAt(i) == 'N') || (input.charAt(i) =='O')){
+                time += 7;
+            }
+            else if((input.charAt(i) == 'P') || (input.charAt(i) == 'Q') || (input.charAt(i) =='R') || (input.charAt(i) =='S')){
+                time += 8;
+            }
+            else if((input.charAt(i) == 'T') || (input.charAt(i) == 'U') || (input.charAt(i) =='V')){
+                time += 9;
+            }
+            else if((input.charAt(i) == 'W') || (input.charAt(i) == 'X') || (input.charAt(i) =='Y') || (input.charAt(i) =='Z')){
+                time += 10;
+            }else{
+                time += 11;
+            }
+        }
+
+        System.out.print(time);
+
+    }
+}
