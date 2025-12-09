ecbafe8602f10d2dea0b9064111bc4629f69701a
imyj00 <108146596+imyj00@users.noreply.github.com>
2025-12-10 02:06:28 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 272 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    static char[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n == 1){
+            System.out.print("*");
+        }else{
+            for(int i = n - 1; i > 0; i--){
+                System.out.print(" ");
+            }
+            System.out.println("*");
+
+            for(int l = 0; l < n-1; l++){
+               for(int k = l; k < n-2; k++){
+                   System.out.print(" ");
+               }
+               System.out.print("*");
+                for(int j = 0; j <= l; j++){
+                    System.out.print(" ");
+                    System.out.print("*");
+                }
+
+                System.out.println();
+
+            }
+
+        }
+
+    }
+
+}
