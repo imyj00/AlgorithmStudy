87e45adaf6496aec7eaf6933ef400e0fdc4b4ae7
imyj00 <108146596+imyj00@users.noreply.github.com>
2026-01-02 18:05:37 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 14832 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        long []A = new long[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        for(int k = 0; k < n; k++){
+            long find = A[k];
+            int i = 0;
+            int j = n - 1;
+
+            //투포인터 알고리즘
+            while(i < j){
+                if(A[i] + A[j] == find){
+                    if( i != k && j != k){
+                        result++;
+                        break;
+                    }else if( i == k){
+                        i++;
+                    }else if( j == k ){
+                        j--;
+                    }
+                }else if(A[i] + A[j] < find){
+                    i++;
+                }else{
+                    j--;
+                }
+            }
+        }
+
+        System.out.println(result);
+        br.close();
+
+
+    }
+}
