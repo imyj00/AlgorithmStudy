b722a05538c3f6647585b71dc8bb83fdd1d8c2aa
imyj00 <108146596+imyj00@users.noreply.github.com>
2026-06-07 15:53:02 +0900

    [level 0] Title: 문자열 섞기, Time: 1.04 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        for(int i = 0; i < str1.length(); i++){
+            answer += Character.toString(str1.charAt(i));
+            answer += Character.toString(str2.charAt(i));
+        }
+        
+        
+        return answer;
+    }
+}
